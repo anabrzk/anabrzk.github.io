01936cc96a24ab6ae13b2c14d8fa7fb32c6f8963
Anastacia <nastyaberezyuk8@gmail.com>
2019-09-12 12:58:57 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var scroll = new SmoothScroll('a[href*="#"]');
+
+
+$('.more-btn').click(function() {
+  $('#hiden-gallery').toggleClass('hide');
+  $('#hiden-gallery').toggleClass('open');
+  if ( $('#hiden-gallery').is( ".open" ) ) {
+    $(".more-btn-inside").text("Show Less.");
+  }else {
+    $(".more-btn-inside").text("Show More.");
+  }
+});
+/*function slickify(){
+  $('.blog-slider').slick({
+      autoplay: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+            breakpoint: 991,
+            settings: "unslick"
+        }
+      ] 
+  });
+  $(".slick-next").text("");
+  $(".slick-next").addClass("icofont-long-arrow-right");
+  $(".slick-prev").text("");*/
+  $(".slick-prev").addClass("icofont-long-arrow-left");
+}
+
+
+
+
+
